42443fbe28fe7ebf6f32caeae17d5e8ee42c4506
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-22 16:16:17 +0100

    Further updates in the abc test

--- test/tests_abc.jl
+++ test/tests_abc.jl
@@ -10,6 +10,10 @@ Distributions.rand(rng::AbstractRNG, d::UnknownContinuousDistribution) = nothing
 # While the pdf is also unknown, a good summary statistics should be able to proxy it to some extent - the latter is computed externally within a @model macro, and stored in `d`
 Distributions.logpdf(d::UnknownContinuousDistribution, x::Real) = d.summary_statistics_value;
 
+# Bounds
+Distributions.minimum(d::UnknownContinuousDistribution) = -Inf;
+Distributions.maximum(d::UnknownContinuousDistribution) = 0;
+
 """
 This test estimates the mean and standard deviation of normally distributed data.
 
@@ -18,15 +22,15 @@ The data is such that each y_{i} ~ N(μ, σ^2).
 
 # Priors
 - μ ~ Normal(0, λ^2)
-- σ ~ TruncatedNormal(0, λ^2, 0, Inf)
+- σ ~ InverseGamma(3, 1)
 """
 
 # Declare Turing model
-@model function univariate_normal_model(y::Vector{Float64}, λ::Float64; no_simulations::Int64=1, percentile::Float64=0.1)
+@model function univariate_normal_model(y::Vector{Float64}, λ::Float64; no_simulations::Int64=1, percentile::Float64=0.25)
     
     # Priors
     μ ~ Normal(0, λ);
-    σ ~ TruncatedNormal(0, λ, 0, Inf);
+    σ ~ InverseGamma(3, 1);
 
     # Data percentiles
     percentiles_y = quantile(y, percentile:percentile:1);
@@ -54,27 +58,55 @@ The data is such that each y_{i} ~ N(μ, σ^2).
 end
 
 # Test function
-function univariate_normal_test(N::Int64, M::Int64; μ0::Float64=0.0, σ0::Float64=1.0, λ::Float64=100.0)
+function univariate_normal_test(N::Int64, M::Int64; μ0::Float64=0.0, σ0::Float64=1.0, λ::Float64=100.0, noise::Float64=0.0)
 
     # Set seed for reproducibility
     Random.seed!(1);
 
     # Memory pre-allocation for output
     simulations_output = Vector{Chains}();
+    simulations_output_avgs = zeros(2, M);
 
-    # Loop over replications
+    @info("Looping over replications");
+
     for i=1:M
         
         # Current simulated data
         y_i = μ0 .+ σ0*randn(N);
         
+        # Initial value for the model parameters
+        init_θ = [0; 1/2] + noise*[randn(); abs(randn())];
+        
+        # Current simulation output
+        current_simulation_output = sample(
+            univariate_normal_model(y_i, λ), 
+            MH(0.01*Matrix(I, 2, 2)), 
+            50000, discard_initial=25000, 
+            init_params=init_θ # somewhere close to the priors' mean
+        );
+
         # Update `simulations_output`
-        push!(simulations_output, sample(univariate_normal_model(y_i, λ), MH(0.01*Matrix(I, 2, 2)), 50000, discard_initial=25000));
+        push!(simulations_output, current_simulation_output);
+
+        # Update `simulations_output_avgs`
+        simulations_output_avgs[:, i] = mean(current_simulation_output).nt[:mean];
     end
 
+    @info("Done!");
+
     # Return output
-    return simulations_output;
+    return simulations_output, simulations_output_avgs;
 end
 
-output=univariate_normal_test(200, 1);
-plot(output[1])
+# Get output
+output_1, output_avg_1 = univariate_normal_test(200, 100);
+output_2, output_avg_2 = univariate_normal_test(200, 100, noise=1.0);
+output_3, output_avg_3 = univariate_normal_test(200, 100, noise=2.5);
+output_4, output_avg_4 = univariate_normal_test(200, 100, noise=5.0);
+
+# Select simulation
+p1 = histogram(output_avg_1', labels=["μ" "σ"])
+p2 = histogram(output_avg_2', labels=["μ" "σ"])
+p3 = histogram(output_avg_3', labels=["μ" "σ"])
+p4 = histogram(output_avg_4', labels=["μ" "σ"])
+plot(p1, p2, p3, p4);
