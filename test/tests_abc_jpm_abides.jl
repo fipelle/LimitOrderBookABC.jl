196bb5445acb1a478cca527b7ba1b73122bb6ae6
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-23 20:31:11 +0100

    Added first draft for abides_model

--- test/tests_abc_jpm_abides.jl
+++ test/tests_abc_jpm_abides.jl
@@ -1,6 +1,13 @@
-include("../../AbidesMarkets.jl/src/AbidesMarkets.jl");
-using Main.AbidesMarkets;
+using MessyTimeSeries;
+using Distances, LinearAlgebra, Random, Statistics, StatsPlots, Turing;
+include("../src/TuringABC.jl"); using Main.TuringABC;
+include("../../AbidesMarkets.jl/src/AbidesMarkets.jl"); using Main.AbidesMarkets;
 
+"""
+Estimate the coefficients used for simulating data through ABIDES-Markets.
+"""
+
+# Shortcut to simulate through ABIDES
 function generate_abides_simulation(build_config_kwargs::NamedTuple; nlevels::Int64=10)
 
     # Build runnable configuration
@@ -16,22 +23,56 @@ function generate_abides_simulation(build_config_kwargs::NamedTuple; nlevels::In
     return AbidesMarkets.get_L2_snapshots(order_book, nlevels);
 end
 
-"""
-    abides_test(M::Int64; build_config_specifics::NamedTuple = NamedTuple())
+# Declare Turing model
+@model function abides_model(seed::Int64, L2_data::JMatrix{Float64})
+    
+    # Priors
+    μ ~ Normal(0, 10_000);  # r_bar
+    κ ~ InverseGamma(3, 1); # kappa
+    σ ~ InverseGamma(3, 1); # fund_vol
+    
+    # Simulate data
+    L2_simulated = generate_abides_simulation((seed=seed, r_bar=μ, kappa=κ, fund_vol=σ));
 
-Estimate the coefficients used for simulating data through ABIDES-Markets.
-"""
+    # Aggregate data
+    L2_data_per_minute = aggregate_L2_eop(L2_data, Minute(1));
+    L2_simulated_per_minute = aggregate_L2_eop(L2_simulated, Minute(1));
+
+    # Compute summary statistics
+    # [TBA]
+
+    # Target
+    for i in axes(y, 1)
+        y[i] ~ UnknownContinuousDistribution(-summary_statistics_value, -Inf, 0.0);
+    end
+end
+
+# Declare test function
 function abides_test(M::Int64; build_config_specifics::NamedTuple = NamedTuple())
 
-    # AbidesMarkets.build_config relevant keyword arguments
-    build_config_kwargs = (
-        seed = 0,
-        build_config_specifics...,
-    );
-    
-    # Generate two simulations
-    L2_1 = generate_abides_simulation(build_config_kwargs);
-    L2_2 = generate_abides_simulation(build_config_kwargs);
+    # Set seed for reproducibility
+    Random.seed!(1);
 
-    return L2_1, L2_2;
+    # Memory pre-allocation for output
+    simulations_output = Vector{Chains}();
+    simulations_output_avgs = zeros(2, M);
+
+    @info("Looping over replications");
+
+    for i=1:M
+
+        # Generate L2 through ABIDES-Markets
+        L2_data = generate_abides_simulation((; seed=i, build_config_specifics...));
+
+        # Current simulation output
+        current_simulation_output = sample(
+            abides_model(i, L2_data),
+            MH(0.01*Matrix(I, 2, 2)), 
+            50000, 
+            discard_initial=25000
+        );
+
+    end
+    
+    return simulations_output, simulations_output_avgs;
 end
